a5ff151085137b581bdc0ef73018e862feb5adcc
qossiim <qosimtoirov2000@gmail.com>
2026-04-27 22:18:39 +0900

    faet: Socket Connection & Batch Server development

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -8,6 +8,7 @@ import { AppResolver } from './app.resolver';
 import { ComponentsModule } from './components/components.module';
 import { DatabaseModule } from './database/database.module';
 import { T } from './libs/types/common';
+import { SocketModule } from './socket/socket.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), GraphQLModule.forRoot({
@@ -25,7 +26,7 @@ import { T } from './libs/types/common';
 				return graphQLFormattedError;
 			},
   }), ComponentsModule, // HTTP
-   DatabaseModule, // TCP
+   DatabaseModule, SocketModule, // TCP
 ],
   controllers: [AppController],
   providers: [AppService, AppResolver],
